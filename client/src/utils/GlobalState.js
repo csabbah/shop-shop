c24c86a5d644573ed2a3729c6446c8f5a186c0f0
Csabbah <csabbah@ryerson.ca>
2022-07-13 09:23:31 -0400

    Cart component now uses reducer to allow users to open and close the cart component - Updates global state

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,5 +1,5 @@
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+import React, { createContext, useContext } from 'react';
+import { useProductReducer } from './reducers';
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
@@ -7,6 +7,8 @@ const { Provider } = StoreContext;
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useProductReducer({
     products: [],
+    cart: [],
+    cartOpen: false,
     categories: [],
     currentCategory: '',
   });
