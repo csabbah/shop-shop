2f0e513023ecea825f19a6cba1b0c33f4e9621cf
Csabbah <csabbah@ryerson.ca>
2022-07-14 12:30:27 -0400

    Upon successful payment, Users will now be redirected to a new Success page and then redirected agan to the home page. Users can now also view their previous order history

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,25 +7,25 @@ const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 const resolvers = {
   Query: {
     checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
       const order = new Order({ products: args.products });
-      const { products } = await order.populate('products');
       const line_items = [];
 
+      const { products } = await order.populate('products');
+
       for (let i = 0; i < products.length; i++) {
-        // generate product id
         const product = await stripe.products.create({
           name: products[i].name,
           description: products[i].description,
+          images: [`${url}/images/${products[i].image}`],
         });
 
-        // generate price id using the product id
         const price = await stripe.prices.create({
           product: product.id,
           unit_amount: products[i].price * 100,
           currency: 'usd',
         });
 
-        // add price id to the line items array
         line_items.push({
           price: price.id,
           quantity: 1,
@@ -36,9 +36,8 @@ const resolvers = {
         payment_method_types: ['card'],
         line_items,
         mode: 'payment',
-        success_url:
-          'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url: 'https://example.com/cancel',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`,
       });
 
       return { session: session.id };
